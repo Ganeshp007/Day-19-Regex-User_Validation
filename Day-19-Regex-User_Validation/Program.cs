09659c7178313525b85c530863c4522f3293f686
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-17 11:43:07 +0530

    UC1_Valid_USerFirstName

---
+++ Day-19-Regex-User_Validation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_19_Regex_User_Validation;
+
+Console.WriteLine("----- Regex User Validation -----\n");
+
+RegexOps pattern = new RegexOps();
+
+Console.Write("Enter the First Name of the User :- ");
+string username=Console.ReadLine();
+if(pattern.ValidateUserName(username))
+{
+    Console.WriteLine("\n>> UserName format is Valid....");
+}
+else
+{
+    Console.WriteLine("\n>> UserName format is Invalid....");
+}
---
+++ Day-19-Regex-User_Validation/RegexOps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_19_Regex_User_Validation
+{
+    internal class RegexOps
+    {
+        public static string RegexRule = "^[A-Z][A-Za-z]{3}";
+
+        public bool ValidateUserName(string username)
+        {
+            {
+                return Regex.IsMatch(username, RegexRule);
+
+            }
+        }
+    }
+}
